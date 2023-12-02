583800f20e59c9f03f1bb9863bcbb89ba4df9470
arthraw <arthursallesandrade@gmail.com>
2023-12-01 23:14:36 -0300

    feat: Create User entity

---
+++ app/src/main/java/com/project/loginscreen/data/model/entities/UserEntity.kt
@@ -0,0 +1,14 @@
+package com.project.loginscreen.data.model.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity
+data class UserEntity(
+    @PrimaryKey val userId : String = UUID.randomUUID().toString(),
+    val name: String,
+    val email : String,
+    val password : String,
+    val birthDate : Long?
+)

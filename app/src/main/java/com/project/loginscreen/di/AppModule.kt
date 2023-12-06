08655b246418a5f9914a2ebec1d5c45ed5f3db81
arthraw <arthursallesandrade@gmail.com>
2023-12-06 00:12:04 -0300

    feat: DI for Use cases

--- app/src/main/java/com/project/loginscreen/di/AppModule.kt
+++ app/src/main/java/com/project/loginscreen/di/AppModule.kt
@@ -5,6 +5,10 @@ import androidx.room.Room
 import com.project.loginscreen.data.model.LoginScreenDataBase
 import com.project.loginscreen.data.model.dao.UserDao
 import com.project.loginscreen.data.repository.UserDaoImpl
+import com.project.loginscreen.presentation.user.use_cases.AddUser
+import com.project.loginscreen.presentation.user.use_cases.EditUser
+import com.project.loginscreen.presentation.user.use_cases.ShowUser
+import com.project.loginscreen.presentation.user.use_cases.UserUseCases
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,5 +33,15 @@ object AppModule {
     @Singleton
     fun provideUserDao(db : LoginScreenDataBase) : UserDao {
         return UserDaoImpl(db.userDao())
+    }
+
+    @Provides
+    @Singleton
+    fun provideUseCases(dao: UserDao) : UserUseCases {
+        return UserUseCases(
+            addUser = AddUser(dao),
+            editUser = EditUser(dao),
+            showUser = ShowUser(dao)
+        )
     }
 }

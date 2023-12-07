80edf399096140625ad46a7a2a329a03513682b3
arthraw <arthursallesandrade@gmail.com>
2023-12-07 00:11:03 -0300

    feat: New use case for users

---
+++ app/src/main/java/com/project/loginscreen/presentation/user/use_cases/SearchUser.kt
@@ -0,0 +1,15 @@
+package com.project.loginscreen.presentation.user.use_cases
+
+import com.project.loginscreen.data.model.dao.UserDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class SearchUser @Inject constructor(
+    private val dao: UserDao
+) {
+
+    suspend operator fun invoke(name : String) : String = withContext(Dispatchers.IO) {
+        return@withContext dao.checkUser(name)
+    }
+}

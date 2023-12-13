ba299492e461d6e498e99502e6f39d8986222469
arthraw <arthursallesandrade@gmail.com>
2023-12-13 00:27:48 -0300

    feat: Use cases adjust

--- app/src/main/java/com/project/loginscreen/presentation/user/use_cases/ComparePass.kt
+++ app/src/main/java/com/project/loginscreen/presentation/user/use_cases/ComparePass.kt
@@ -1,6 +1,8 @@
 package com.project.loginscreen.presentation.user.use_cases
 
+import androidx.lifecycle.LiveData
 import com.project.loginscreen.data.model.dao.UserDao
+import com.project.loginscreen.data.model.entities.UserEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -8,7 +10,7 @@ import javax.inject.Inject
 class ComparePass @Inject constructor(
     private val dao: UserDao
 ) {
-    suspend operator fun invoke(password: String): String? = withContext(Dispatchers.IO) {
-        return@withContext dao.checkPass(password)
+    suspend operator fun invoke(name :String, password: String): LiveData<Map<String,String>> = withContext(Dispatchers.IO) {
+        return@withContext dao.checkPass(name, password)
     }
 }
